911c5337bd34ef2527c20334a3d621d9924b9797
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-23 14:22:30 +0530

    23/2/24

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:practice_project/row_col_stack/practice.dart';
 import 'package:practice_project/stack_widget.dart';
+import 'package:practice_project/text_fromfid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const StackWidget(),
+      home: const TextFromFild(),
       // home: PracticeScreen(),
     );
   }
