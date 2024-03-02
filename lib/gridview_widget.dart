236698733854736009e1d2eda35c6c1ab0e49409
vijay vekariya <vijayvekariya007@gmail.com>
2024-03-02 14:27:02 +0530

    2-3-24

--- lib/gridview_widget.dart
+++ lib/gridview_widget.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-class GridviewWidget extends StatelessWidget {
+class GridviewWidget extends StatefulWidget {
   const GridviewWidget({super.key});
 
+  @override
+  State<GridviewWidget> createState() => _GridviewWidgetState();
+}
+
+class _GridviewWidgetState extends State<GridviewWidget> {
+  List<String> image = [
+    'images/facebook (3) 1.png',
+    'images/21369390.jpg',
+    'images/png-transparent-whatsapp-logo-computer-icons-whatsapp-whatsapp-text-android-symbol-thumbnail.png',
+    'images/facebook (3) 1.png',
+    'images/facebook (3) 1.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +22,7 @@ class GridviewWidget extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: GridView.builder(
-          itemCount: 10,
+          itemCount: image.length,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 4,
             // maxCrossAxisExtent: 200,
@@ -21,9 +33,49 @@ class GridviewWidget extends StatelessWidget {
           itemBuilder: (context, index) {
             return Card(
               color: Colors.white,
-              child: Image(image: AssetImage('images/facebook (3) 1.png')),
+              child: Image(image: AssetImage(image[index])),
             );
           }),
+      // body: GridView.extent(
+      //   // primary: false,
+      //   padding: const EdgeInsets.all(10),
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      //   maxCrossAxisExtent: 150,
+
+      //   children: <Widget>[
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[100],
+      //       child: const Text("He'd have you all unravel at the"),
+      //     ),
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[200],
+      //       child: const Text('Heed not the rabble'),
+      //     ),
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[300],
+      //       child: const Text('Sound of screams but the'),
+      //     ),
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[400],
+      //       child: const Text('Who scream'),
+      //     ),
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[500],
+      //       child: const Text('Revolution is coming...'),
+      //     ),
+      //     Container(
+      //       padding: const EdgeInsets.all(8),
+      //       color: Colors.teal[600],
+      //       child: const Text('Revolution, they...'),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
